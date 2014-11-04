060fc4c3cb09657c5007ee5142eb78faa61e7019
dint <fahrudin1705@gmail.com>
2014-11-04 16:23:01 +0700

    prak7

---
+++ Prak7_Mochammad Fahrudin_4513210024_P1/ascenganjil.java
@@ -0,0 +1,23 @@
+//Program 01C
+import java.util.Scanner;
+public class ascenganjil {
+public static void main(String[] args) {
+ int i;
+ int j;
+ Scanner masukan = new Scanner(System.in);
+ System.out.print("Masukkan batas bilangannya : ");
+ int batas = masukan.nextInt();
+ for(i=1;i<=batas;i++)
+ {
+  for(j=1;j<=i;j++)
+  {
+  if(i%2==1){
+   System.out.print(i+" ");
+  
+  }
+  }
+ 
+ System.out.println();
+ }
+}
+} 
---
+++ Prak7_Mochammad Fahrudin_4513210024_P1/nilaidescen.java
@@ -0,0 +1,11 @@
+//Program 01B
+	public class nilaidescen{
+		public static void main (String[] args){							
+			for(int line =9 ; line >= 1; line--){
+					for(int j=1; j <= line; j++){
+			System.out.print(line);
+			}
+				System.out.println("");
+				}
+		}
+}	
